91420763e6b615d03b75126d840d00a933664fdf
diaboloss217 <102517593+diaboloss217@users.noreply.github.com>
2022-08-25 17:14:03 +0900

    Fix: Import 수정

--- src/main/java/com/ujutechnology/api8/biz/repository/ProductRepository.java
+++ src/main/java/com/ujutechnology/api8/biz/repository/ProductRepository.java
@@ -1,5 +1,9 @@
 package com.ujutechnology.api8.biz.repository;
 
+import com.ujutechnology.api8.biz.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
