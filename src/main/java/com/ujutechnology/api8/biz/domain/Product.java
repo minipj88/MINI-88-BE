f50a1c69691d72fb26c21a85a0a4606dac1a3ffc
diaboloss217 <diaboloss217@gmail.com>
2022-08-29 21:56:13 +0900

    refactor: 불필요한 부분 정리, Column명 표시

--- src/main/java/com/ujutechnology/api8/biz/domain/Product.java
+++ src/main/java/com/ujutechnology/api8/biz/domain/Product.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "PRODUCT")
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Data
 public class Product {
 
@@ -16,28 +16,31 @@ public class Product {
 
     @Column(name = "PRODUCTNAME", nullable = false)
     private String productName; // 금융상품명
-    private int age;    //20-30, 40, 50-60, all 
+    @Column(name = "AGE")
+    private int age;    //20-30, 40, 50-60, all
+    @Column(name = "RATE")
     private double rate;
+    @Column(name = "JOB")
     private String job;
-    private String financialCompanyNumber;
+    @Column(name = "COMPANYNAME")
     private String financialCompanyName;
+    @Column(name = "PRODUCTNUMBER")
     private String productNumber;
+    @Column(name="JOINWAY")
     private String joinWay;
+    @Column(name="CBNAME")
     private String cbName;
-    private String creditProductTypeName;
 
     @Builder
-    public Product(String financialCompanyNumber, String financialCompanyName, String productNumber, String productName, String cbName, String creditProductTypeName, String joinWay, Integer age, Double rate, String job){
-        this.financialCompanyName = financialCompanyName;
-        this.productNumber = productNumber;
+    public Product(Long id, String productName, int age, double rate, String job, String financialCompanyName, String productNumber, String joinWay, String cbName) {
+        this.id = id;
         this.productName = productName;
-        this.cbName = cbName;
-        this.joinWay = joinWay;
         this.age = age;
         this.rate = rate;
         this.job = job;
-        this.creditProductTypeName = creditProductTypeName;
-
+        this.financialCompanyName = financialCompanyName;
+        this.productNumber = productNumber;
+        this.joinWay = joinWay;
+        this.cbName = cbName;
     }
-
 }
